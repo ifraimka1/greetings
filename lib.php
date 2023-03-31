43987285a156691c76d7e4ec3145ca12d0c18c88
ifraim <60849826+ifraimka1@users.noreply.github.com>
2023-03-31 17:22:49 +0300

    Добавление ссылки на плагин в навигацию
    Ссылка на плагин была добавлена в боковое меню навигации и в меню навигации домашней страницы.

--- lib.php
+++ lib.php
@@ -64,3 +64,38 @@ function local_greetings_get_greeting($user) {
 
     return get_string($langstr, 'local_greetings', fullname($user));
 }
+
+/**
+ * Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in the navigation tree.
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
+    $frontpage->add(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/message', '')
+    );
+}
+
+/**
+ * Insert a link to index.php on the navigation drawer.
+ *
+ * @param global_navigation $root
+ */
+function local_greetings_extend_navigation(global_navigation $root) {
+    $node = navigation_node::create(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/message', '')
+    );
+
+    $node->showinflatnavigation = true;
+    $root->add_node($node);
+}
